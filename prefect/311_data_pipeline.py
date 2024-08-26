a49b9c1fcce1a621176378d8e5af2639aa7ba8ee
annieycchiu <annieycchiu@gmail.com>
2024-08-25 22:53:06 -0700

    GCP cloud run push work pool

--- prefect/311_data_pipeline.py
+++ prefect/311_data_pipeline.py
@@ -1,44 +1,14 @@
-import os
+from datetime import datetime, timedelta
+
 from prefect import task, flow
+from prefect.blocks.system import Secret
+from prefect.deployments import DeploymentImage
+
+from google.cloud import bigquery, storage
 from sodapy import Socrata
-from google.cloud import bigquery
-from google.oauth2 import service_account
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-# Define the fetch_data task
-@task
-def fetch_data(client, data_limit=100):
-    data = client.get(
-        dataset_identifier='vw6y-z8j6', 
-        limit=data_limit
-    )
-    return data
-
-# Define the transform_data task
-@task
-def transform_data(data):
-    needed_keys = [
-        'service_request_id', 'requested_datetime', 'closed_date', 'updated_datetime', 'status_description', 'status_notes',
-        'agency_responsible', 'service_name', 'service_subtype', 'service_details', 'address', 'street', 'supervisor_district',
-        'neighborhoods_sffind_boundaries', 'police_district', 'lat', 'long', 'source'
-    ]
-    filtered_data = []
-    for d in data:
-        filtered_dict = {} 
-        for k, v in d.items():
-            if k in needed_keys: 
-                if v is not None and not isinstance(v, str):
-                    v = str(v)
-                filtered_dict[k] = v 
-        filtered_data.append(filtered_dict) 
-    return filtered_data
-
-# Define the insert_record_one_by_one task
-@task
-def insert_record_one_by_one(client, table_id, records):
+# insert record into Bigquery
+def insert_record_one_by_one(client, table_id, records, print_successful_records=True):
     failed_records = []
     
     for record in records:
@@ -47,57 +17,108 @@ def insert_record_one_by_one(client, table_id, records):
         if errors:
             failed_records.append({'record': record, 'errors': errors})
         else:
-            print(f"Record inserted successfully: {record}")
+            if print_successful_records:
+                print(f"Record inserted successfully: {record}")
     
     return failed_records
 
-# Define the load_data_to_bigquery task
 @task
-def load_data_to_bigquery(client, data):
-    table_id = 'my-tenth-project-432516.test_dataset.311_data'
-    failed_records = insert_record_one_by_one(client, table_id, data)
+async def fetch_secret(name):
+    # Load the secret asynchronously
+    secret_block = await Secret.load(name)
+    return secret_block.get()
+
+@task(cache_expiration=timedelta(hours=1))
+def fetch_data(client, data_limit=100):
+    data = client.get(
+        dataset_identifier='vw6y-z8j6', 
+        limit=data_limit
+    )
+    return data
+
+@task
+def transform_data(data):
+    needed_keys = [
+        'service_request_id', 'requested_datetime', 'closed_date', 'updated_datetime', 'status_description', 
+        'status_notes', 'agency_responsible', 'service_name', 'service_subtype', 'service_details', 'address', 
+        'street', 'supervisor_district', 'neighborhoods_sffind_boundaries', 'police_district', 'lat', 'long', 'source'
+    ]
+    filtered_data = []
+    for d in data:
+        # retrive needed data
+        filtered_dict = {} 
+        for k, v in d.items():
+            if k in needed_keys: 
+                # convert datatype of values to string
+                if v is not None and not isinstance(v, str):
+                    v = str(v)
+                filtered_dict[k] = v 
+        filtered_data.append(filtered_dict) 
+    return filtered_data
+
+@task
+def load_data_to_bigquery(bq_client, bq_table_id, gcs_client, bucket_name, data):
+    failed_records = insert_record_one_by_one(bq_client, bq_table_id, data)
     
     if failed_records:
-        with open('failed_records_report.txt', 'w') as f:
-            for entry in failed_records:
-                f.write(f"Record: {entry['record']}\n")
-                f.write(f"Errors: {entry['errors']}\n")
-                f.write("\n---\n\n")
-        print("Report of failed records has been written to 'failed_records_report.txt'.")
+        report_name = f"failed_records_report_{datetime.today().strftime('%Y-%m-%d')}.txt"
+        report_content = ""
+        
+        for entry in failed_records:
+            report_content += f"Record: {entry['record']}\n"
+            report_content += f"Errors: {entry['errors']}\n"
+            report_content += "\n---\n\n"
+
+        bucket = gcs_client.bucket(bucket_name)
+        blob = bucket.blob(report_name)
+        blob.upload_from_string(report_content)
+        
+        print(f"Report of failed records has been uploaded to GCP bucket '{bucket_name}' as '{report_name}'.")
+
     else:
         print("All records were inserted successfully.")
 
-# Set up Socrata client
-@task
-def setup_socrata_client():
-    return Socrata(
-        'data.sfgov.org',
-        app_token=os.getenv('SOCRATA_APP_TOKEN'),
-        username=os.getenv('SOCRATA_USERNAME'),
-        password=os.getenv('SOCRATA_PASSWORD')
-    )
-
-# Set up BigQuery client
-@task
-def setup_bigquery_client():
-    key_path = os.getenv('GCP_ACCESS_KEY') 
-    credentials = service_account.Credentials.from_service_account_file(key_path)
-    return bigquery.Client(
-        credentials=credentials, 
-        project=credentials.project_id
-    )
 
 # Define the Prefect flow
-@flow
-def etl_flow(data_limit=10):
-    socrata_client = setup_socrata_client()
-    bigquery_client = setup_bigquery_client()
+@flow(name='311 data ETL', log_prints=True)
+async def etl_flow(data_limit=10):
+    # Load the secret
+    socrata_app_token = await fetch_secret('socrata-app-token')
+    socrata_username = await fetch_secret('socrata-username')
+    socrata_password = await fetch_secret('socrata-password')
     
+    # Initialize clients
+    socrata_client = Socrata(
+        'data.sfgov.org', 
+        app_token=socrata_app_token,
+        username=socrata_username,
+        password=socrata_password
+        )
+    bigquery_client = bigquery.Client()
+    storage_client = storage.Client()
+    bigquery_table_id = 'my-tenth-project-432516.test_dataset.311_data'
+    bucket_name = 'my-prefect-bucket'
+    
+    # Run tasks
     data = fetch_data(socrata_client, data_limit)
     transformed_data = transform_data(data)
-    load_data_to_bigquery(bigquery_client, transformed_data)
+    load_data_to_bigquery(
+        bigquery_client, bigquery_table_id, 
+        storage_client, bucket_name, 
+        transformed_data
+        )
+
+    print('Complete the task!')
 
 # Run the flow
 if __name__ == "__main__":
-    etl_flow()
+    etl_flow.deploy(                                                            
+        name='311-data-etl-deployment',
+        work_pool_name='my-cloud-run-pool',
+        cron='0 1 * * *',
+        image=DeploymentImage(
+            name='prefect-etl-image:latest',
+            platform='linux/amd64',
+        )
+    )
 
